104abc3ef03d67fa831c7bd8b975cbb96a222b22
Mingyu Choi <mg010626@naver.com>
2023-05-19 15:05:15 +0900

    add getHashKey on MainActivity

--- app/src/main/java/com/example/plantogether/MainActivity.kt
+++ app/src/main/java/com/example/plantogether/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.plantogether
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.example.plantogether.databinding.ActivityMainBinding
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
@@ -11,9 +17,29 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initLayout()
+        getHashKey()
     }
 
     private fun initLayout() {
 
+    }
+
+    private fun getHashKey() {
+        try {
+            val information =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            val signatures = information.signingInfo.apkContentsSigners
+            val md = MessageDigest.getInstance("SHA")
+            for (signature in signatures) {
+                val md: MessageDigest
+                md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                var hashcode = String(Base64.encode(md.digest(), 0))
+                Log.d("hashcode", "" + hashcode)
+            }
+        } catch (e: Exception) {
+            Log.d("hashcode", "에러::" + e.toString())
+
+        }
     }
 }
