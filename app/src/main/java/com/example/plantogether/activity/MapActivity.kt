3ddf436d31238828605cb6231132e931ad9696c0
Mingyu Choi <mg010626@naver.com>
2023-05-29 01:42:03 +0900

    modify MapActivity.kt and activity_map.xml

--- app/src/main/java/com/example/plantogether/activity/MapActivity.kt
+++ app/src/main/java/com/example/plantogether/activity/MapActivity.kt
@@ -12,26 +12,62 @@ import com.naver.maps.map.OnMapReadyCallback
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var binding: ActivityMapBinding
-    // lateinit var mapView: MapView
-    // lateinit var naverMap: NaverMap
+     lateinit var mapView: MapView
+     lateinit var naverMap: NaverMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // naver map
-        //mapView = binding.mapView
-        //mapView.onCreate(savedInstanceState)
-        //mapView.getMapAsync(this)
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
     }
 
     override fun onMapReady(p0: NaverMap) {
-        // naverMap = p0
+        naverMap = p0
         val cameraPosition = CameraPosition(
             LatLng( 37.5407624841263,
                 127.07654675326717),  // 위치 지정
-            9.0 // 줌 레벨
+            15.0 // 줌 레벨
         )
-        // naverMap.setCameraPosition(cameraPosition)
+        naverMap.setCameraPosition(cameraPosition)
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState!!)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
 }
