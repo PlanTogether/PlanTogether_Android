c52fc640e834c3bc77d29d7d1ea8ed4d58e5c188
Mingyu Choi <mg010626@naver.com>
2023-05-29 02:46:44 +0900

    modify MapActivity.kt

--- app/src/main/java/com/example/plantogether/activity/MapActivity.kt
+++ app/src/main/java/com/example/plantogether/activity/MapActivity.kt
@@ -1,19 +1,34 @@
 package com.example.plantogether.activity
-
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.plantogether.R
 import com.example.plantogether.databinding.ActivityMapBinding
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.CameraPosition
-import com.naver.maps.map.MapView
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
+import java.io.IOException
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var binding: ActivityMapBinding
-     lateinit var mapView: MapView
-     lateinit var naverMap: NaverMap
+    lateinit var mapView: MapView
+    lateinit var naverMap: NaverMap
+
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+    lateinit var locationSource: FusedLocationSource
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
@@ -25,49 +40,101 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: NaverMap) {
-        naverMap = p0
+    override fun onMapReady(navermap: NaverMap) {
+        naverMap = navermap
         val cameraPosition = CameraPosition(
-            LatLng( 37.5407624841263,
-                127.07654675326717),  // 위치 지정
+            LatLng(
+                37.5407624841263, 127.07654675326717
+            ),  // 위치 지정
             15.0 // 줌 레벨
         )
         naverMap.setCameraPosition(cameraPosition)
+
+        val marker = Marker()
+        marker.position = LatLng(
+            naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude
+        )
+        marker.icon = OverlayImage.fromResource(R.drawable.icon_marker_black)
+        marker.map = naverMap
+
+
+
+        naverMap.addOnCameraChangeListener { reason, animated ->
+            Log.i("NaverMap", "카메라 변경 - reson: $reason, animated: $animated")
+            marker.position = LatLng(
+                // 현재 보이는 네이버맵의 정중앙 가운데로 마커 이동
+                naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude
+            )
+        }
+
+        naverMap.addOnCameraIdleListener {
+            marker.position = LatLng(
+                naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude
+            )
+        }
+
+
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+
+        var currentLocation: Location?
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            currentLocation = location
+            // 위치 오버레이의 가시성은 기본적으로 false로 지정되어 있습니다.
+            // 가시성을 true로 변경하면 지도에 위치 오버레이가 나타납니다.
+            // 파랑색 점, 현재 위치 표시
+            naverMap.locationOverlay.run {
+                isVisible = true
+                position = LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+            }
+
+            // 카메라 현재위치로 이동
+            val cameraUpdate = CameraUpdate.scrollTo(
+                LatLng(
+                    currentLocation!!.latitude, currentLocation!!.longitude
+                )
+            )
+            naverMap.moveCamera(cameraUpdate)
+
+            // 마커 현재위치로 변경
+            marker.position = LatLng(
+                naverMap.cameraPosition.target.latitude,
+                naverMap.cameraPosition.target.longitude
+            )
+        }
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
+    // 좌표 -> 주소 변환
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geoCoder = Geocoder(this, Locale.KOREA)
+        val address: ArrayList<Address>
+        var addressResult = "주소를 가져 올 수 없습니다."
+        try {
+            //세번째 파라미터는 좌표에 대해 주소를 리턴 받는 갯수로
+            //한좌표에 대해 두개이상의 이름이 존재할수있기에 주소배열을 리턴받기 위해 최대갯수 설정
+            address = geoCoder.getFromLocation(lat, lng, 1) as ArrayList<Address>
+            if (address.size > 0) {
+                // 주소 받아오기
+                val currentLocationAddress = address[0].getAddressLine(0)
+                    .toString()
+                addressResult = currentLocationAddress
+
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return addressResult
     }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState!!)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
-
 }
