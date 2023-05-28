b8835103502528be7d37a5e7bed9e89f29b2c21f
Mingyu Choi <mg010626@naver.com>
2023-05-29 01:34:09 +0900

    modify MapActivity and add network_security_config

--- app/src/main/java/com/example/plantogether/activity/MapActivity.kt
+++ app/src/main/java/com/example/plantogether/activity/MapActivity.kt
@@ -1,15 +1,37 @@
 package com.example.plantogether.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.plantogether.R
+import androidx.appcompat.app.AppCompatActivity
 import com.example.plantogether.databinding.ActivityMapBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
 
-class MapActivity : AppCompatActivity() {
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     lateinit var binding: ActivityMapBinding
+    // lateinit var mapView: MapView
+    // lateinit var naverMap: NaverMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // naver map
+        //mapView = binding.mapView
+        //mapView.onCreate(savedInstanceState)
+        //mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        // naverMap = p0
+        val cameraPosition = CameraPosition(
+            LatLng( 37.5407624841263,
+                127.07654675326717),  // 위치 지정
+            9.0 // 줌 레벨
+        )
+        // naverMap.setCameraPosition(cameraPosition)
     }
 }
