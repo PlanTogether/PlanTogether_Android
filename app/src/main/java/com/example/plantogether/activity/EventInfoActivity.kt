cb562f742816b7973d19e93cc9c9f41fc0acc022
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2023-05-29 03:10:26 +0900

    fragment에서 activity로 넘어오는거

--- app/src/main/java/com/example/plantogether/activity/EventInfoActivity.kt
+++ app/src/main/java/com/example/plantogether/activity/EventInfoActivity.kt
@@ -1,14 +1,37 @@
 package com.example.plantogether.activity
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
 import com.example.plantogether.databinding.ActivityEventInfoBinding
 import com.example.plantogether.dialog.InviteDialog
+import com.kakao.sdk.common.util.KakaoCustomTabsClient
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
+import com.kakao.sdk.template.model.Link
+import com.kakao.sdk.template.model.TextTemplate
 
 class EventInfoActivity : AppCompatActivity() {
     lateinit var binding: ActivityEventInfoBinding
-    lateinit var date: String
+    var date = ""
+    var ok = 0
+
+    companion object {
+        const val TAG = "EventInfoActivity"
+    }
+
+    val defaultText = TextTemplate(
+        text = """
+        카카오톡 공유는 카카오톡을 실행하여
+        사용자가 선택한 채팅방으로 메시지를 전송합니다.
+    """.trimIndent(),
+        link = Link(
+            webUrl = "https://developers.kakao.com",
+            mobileWebUrl = "https://developers.kakao.com"
+        )
+    )
 
     var fm = supportFragmentManager
     var inviteDialog = InviteDialog()
@@ -18,6 +41,7 @@ class EventInfoActivity : AppCompatActivity() {
         setContentView(binding.root)
         val intent = getIntent()
         date = intent.getStringExtra("date").toString()
+
         initLayout()
         initBtn()
     }
@@ -33,7 +57,55 @@ class EventInfoActivity : AppCompatActivity() {
                 bundle.putString("date", date)
                 inviteDialog.arguments = bundle
                 inviteDialog.show(fm, "dialog")
+
             }
         }
     }
+
+    public fun setOk() {
+        sendMessage()
+    }
+
+    private fun sendMessage() {
+//        if (ShareClient.instance.isKakaoTalkSharingAvailable()) {
+//            // 카카오톡으로 카카오톡 공유 가능
+//            ShareClient.instance.shareDefault(context, defaultFeed) { sharingResult, error ->
+//                if (error != null) {
+//                    Log.e(TAG, "카카오톡 공유 실패", error)
+//                }
+//                else if (sharingResult != null) {
+//                    Log.d(TAG, "카카오톡 공유 성공 ${sharingResult.intent}")
+//                    startActivity(sharingResult.intent)
+//
+//                    // 카카오톡 공유에 성공했지만 아래 경고 메시지가 존재할 경우 일부 컨텐츠가 정상 동작하지 않을 수 있습니다.
+//                    Log.w(TAG, "Warning Msg: ${sharingResult.warningMsg}")
+//                    Log.w(TAG, "Argument Msg: ${sharingResult.argumentMsg}")
+//                }
+//            }
+//        } else {
+//            // 카카오톡 미설치: 웹 공유 사용 권장
+//            // 웹 공유 예시 코드
+//            val sharerUrl = WebSharerClient.instance.makeDefaultUrl(defaultFeed)
+//
+//            // CustomTabs으로 웹 브라우저 열기
+//
+//            // 1. CustomTabsServiceConnection 지원 브라우저 열기
+//            // ex) Chrome, 삼성 인터넷, FireFox, 웨일 등
+//            try {
+//                KakaoCustomTabsClient.openWithDefault(context, sharerUrl)
+//            } catch(e: UnsupportedOperationException) {
+//                // CustomTabsServiceConnection 지원 브라우저가 없을 때 예외처리
+//            }
+//
+//            // 2. CustomTabsServiceConnection 미지원 브라우저 열기
+//            // ex) 다음, 네이버 등
+//            try {
+//                KakaoCustomTabsClient.open(context, sharerUrl)
+//            } catch (e: ActivityNotFoundException) {
+//                // 디바이스에 설치된 인터넷 브라우저가 없을 때 예외처리
+//            }
+//        }
+    }
+
 }
+
--- app/src/main/java/com/example/plantogether/dialog/InviteDialog.kt
+++ app/src/main/java/com/example/plantogether/dialog/InviteDialog.kt
@@ -1,5 +1,6 @@
 package com.example.plantogether.dialog
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -7,17 +8,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
-import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
-import com.example.plantogether.R
+import androidx.fragment.app.setFragmentResult
+import com.example.plantogether.activity.EventInfoActivity
 import com.example.plantogether.databinding.FragmentInviteDialogBinding
+import java.util.Objects
 
-class InviteDialog : DialogFragment() {
+
+class InviteDialog : DialogFragment()  {
+
 
     var date = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         isCancelable = true
     }
 
@@ -33,15 +39,16 @@ class InviteDialog : DialogFragment() {
         binding = FragmentInviteDialogBinding.inflate(inflater,container,false)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
+
         return binding!!.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initBtn()
     }
+
 
     private fun initBtn() {
         binding?.apply {
@@ -49,7 +56,16 @@ class InviteDialog : DialogFragment() {
             inviteText.text = "$date 나 이거 초대 보낼랭"
 
             okBtn.setOnClickListener {
-                Toast.makeText(activity,inviteText.text.toString(),Toast.LENGTH_SHORT).show()
+                (activity as EventInfoActivity).setOk()
+                val fragmentManager = requireActivity().supportFragmentManager
+                fragmentManager.beginTransaction().remove(this@InviteDialog).commit()
+                fragmentManager.popBackStack()
+            }
+
+            cancelBtn?.setOnClickListener {
+                val fragmentManager = requireActivity().supportFragmentManager
+                fragmentManager.beginTransaction().remove(this@InviteDialog).commit()
+                fragmentManager.popBackStack()
             }
         }
     }
