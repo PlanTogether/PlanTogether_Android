08e04d4fb990ef00509bbc3f1125d94b6ae906d4
hayoon0524 <92996412+hayoon0524@users.noreply.github.com>
2023-05-28 23:03:57 +0900

    invite dialog 구현

--- app/src/main/java/com/example/plantogether/activity/EventInfoActivity.kt
+++ app/src/main/java/com/example/plantogether/activity/EventInfoActivity.kt
@@ -3,10 +3,31 @@ package com.example.plantogether.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.plantogether.R
+import com.example.plantogether.databinding.ActivityEventInfoBinding
+import com.example.plantogether.databinding.ActivityMainBinding
 
 class EventInfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEventInfoBinding
+    lateinit var date: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_info)
+        binding = ActivityEventInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val intent = getIntent()
+        date = intent.getStringExtra("date").toString()
+        initLayout()
+        initBtn()
+    }
+    private fun initLayout() {
+        binding.apply {
+            eventDate.text = date
+        }
+    }
+    private fun initBtn() {
+        binding.apply {
+            sendInvitation.setOnClickListener {
+                //dialogue 호출
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/plantogether/dialog/InviteDialog.kt
@@ -0,0 +1,4 @@
+package com.example.plantogether.dialog
+
+class InviteDialog {
+}
