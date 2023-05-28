5b1a8dfc96946c0d6254ad111cacf36650c1a107
Mingyu Choi <mg010626@naver.com>
2023-05-28 17:13:54 +0900

    commit for pull

--- app/src/main/java/com/example/plantogether/MainActivity.kt
+++ app/src/main/java/com/example/plantogether/MainActivity.kt
@@ -6,21 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.plantogether.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayoutMediator
 import java.security.MessageDigest
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
     lateinit var binding:ActivityMainBinding
+    lateinit var bnv: BottomNavigationView
     val textArr =  arrayListOf<String>("캘린더", "이벤트","알림")
-    val imgArr = arrayListOf<Int>(R.drawable.baseline_calendar_today_30_white,
-        R.drawable.baseline_star_outline_30_white, R.drawable.baseline_notifications_none_30_white)
-    val imgSelectedArr = arrayListOf<Int>(R.drawable.baseline_calendar_today_30_black,
-        R.drawable.baseline_star_outline_30_black, R.drawable.baseline_notifications_none_30_black)
+    val imgArr = arrayListOf<Int>(R.drawable.icon_calendar_white,
+        R.drawable.icon_eventstar_white, R.drawable.icon_notification_white)
+    val imgSelectedArr = arrayListOf<Int>(R.drawable.icon_calendar_black,
+        R.drawable.icon_eventstar_black, R.drawable.icon_notification_black)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        bnv = binding.bottomNav
         setContentView(binding.root)
         initLayout()
         getHashKey()
@@ -35,13 +41,42 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initLayout() {
         binding.viewpager.adapter = MyViewPagerAdapter(this)
-        binding.tabLayout.setBackgroundColor(Color.parseColor("#ACDBF6"));
-        TabLayoutMediator(binding.tabLayout, binding.viewpager) {
-                tab,pos ->
-            // tab.text = textArr[pos]
-            tab.setIcon(imgArr[pos])
-        }.attach()
+
+        binding.viewpager.registerOnPageChangeCallback(
+            object: ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNav.menu.getItem(position).isChecked = true
+                }
+            }
+        )
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(this)
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.menuCalendar -> {
+                // ViewPager의 현재 item에 첫 번째 화면을 대입
+                binding.viewpager.currentItem = 0
+                return true
+            }
+            R.id.menuEvent -> {
+                // ViewPager의 현재 item에 두 번째 화면을 대입
+                binding.viewpager.currentItem = 1
+                return true
+            }
+            R.id.menuNotification -> {
+                // ViewPager의 현재 item에 세 번째 화면을 대입
+                binding.viewpager.currentItem = 2
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+
 
     private fun getHashKey() {
         try {
