6596e3ed0bbeb14a1a5800d3cc04c637e69f5958
Mingyu Choi <mg010626@naver.com>
2023-06-20 20:33:21 +0900

    Style | 이벤트 관련 레이아웃 수정

--- app/src/main/java/com/example/plantogether/activity/EditEventActivity.kt
+++ app/src/main/java/com/example/plantogether/activity/EditEventActivity.kt
@@ -80,7 +80,6 @@ class EditEventActivity : AppCompatActivity() {
 
                 CoroutineScope(Dispatchers.IO).launch {
                     val setEventDataTask = async { rdb.child(id).setValue(newEventData).await() }
-                    val setEventDataTasks = mutableListOf<Deferred<Unit>>()
 
                     for (invitee in event.participantName) {
                         if (invitee != userName) {
@@ -91,9 +90,6 @@ class EditEventActivity : AppCompatActivity() {
                     }
 
                     setEventDataTask.await() // 메인 이벤트 데이터 설정이 완료될 때까지 기다립니다.
-
-                    // 모든 초대자 이벤트 데이터가 설정될 때까지 기다립니다.
-                    // setEventDataTasks.awaitAll()
 
                     withContext(Dispatchers.Main) {
                         val editintent = Intent()
